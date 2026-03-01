a5bb3d1cc8f4213a066c3e0b68c64db3d698bce3
peter <peter@gmail.com>
2026-03-01 19:14:53 +0100

    reduction ls

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -2,7 +2,7 @@ return
 {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-        ensure_installed = { "lua_ls", "csharp_ls", "jsonls", "yamlls", "html", "ts_ls" },
+        ensure_installed = { "lua_ls", "csharp_ls" },
     },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
